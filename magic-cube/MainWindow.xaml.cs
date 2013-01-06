46cfe39340859f0cd53d132632ee1743cbe869a9
Barbu Paul - Gheorghe <barbu.paul.gheorghe@gmail.com>
2013-01-07 00:08:26 +0200

    RubikCube class allows the creation of a bigger cube out of small ones

--- magic-cube/MainWindow.xaml.cs
+++ magic-cube/MainWindow.xaml.cs
@@ -24,31 +24,12 @@ namespace magic_cube {
         }
 
         private void Window_Loaded(object sender, RoutedEventArgs e) {
-            int edge_len = 1;
-            int size = 5; // the cube will be size x size x size
-            double space = 0.1; //space between the cubes forming the bigger cube
+            RubikCube c = new RubikCube(2, new Point3D(0, 0, 0), 3, 0.05);
 
-            Cube c;
             ModelVisual3D m;
-
-            Point3D o = new Point3D();
-            o.X = o.Y = o.Z = 0;
-
-            for(int y = 0; y<size; y++){
-                for (int z = 0; z < size; z++) {
-                    for (int x = 0; x < size; x++) {
-                        c = new Cube(new Point3D(o.X + (edge_len + space) * x, o.Y + (edge_len + space) * y, o.Z + (edge_len + space) * z), edge_len, new Dictionary<CubeFace, Material>() {
-                            {CubeFace.F, new DiffuseMaterial(new SolidColorBrush(Colors.White))},
-                            {CubeFace.R, new DiffuseMaterial(new SolidColorBrush(Colors.Blue))},
-                            {CubeFace.U, new DiffuseMaterial(new SolidColorBrush(Colors.Yellow))},
-                        });
-
-                        m = new ModelVisual3D();
-                        m.Content = c.group;
-                        this.mainViewport.Children.Add(m);
-                    }
-                }
-            }
+            m = new ModelVisual3D();
+            m.Content = c.group;
+            this.mainViewport.Children.Add(m);
         }
     }
 }
---
+++ magic-cube/RubikCube.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Media.Media3D;
+using System.Windows.Media;
+
+namespace magic_cube {
+    public class RubikCube : Cube {
+        /// <summary>
+        /// The cube will be size x size x size
+        /// </summary>
+        private int size;
+
+        private Point3D origin;
+
+        /// <summary>
+        /// Length of the cube edge
+        /// </summary>
+        private double edge_len;
+
+        /// <summary>
+        /// Space between the cubes forming the bigger cube
+        /// </summary>
+        private double space;
+
+        public RubikCube(int size, Point3D o, double len = 1, double space = 0.1) {
+            this.size = size;
+            this.origin = o;
+            this.edge_len = len;
+            this.space = space;
+            group = new Model3DGroup();
+
+            createCube();
+        }
+
+        protected override void createCube(){
+            Cube c;
+
+            for (int y = 0; y < size; y++) {
+                for (int z = 0; z < size; z++) {
+                    for (int x = 0; x < size; x++) {
+                        c = new Cube(new Point3D(origin.X + (edge_len + space) * x, origin.Y + (edge_len + space) * y, origin.Z + (edge_len + space) * z), edge_len, new Dictionary<CubeFace, Material>() {
+                            {CubeFace.F, new DiffuseMaterial(new SolidColorBrush(Colors.White))},
+                            {CubeFace.R, new DiffuseMaterial(new SolidColorBrush(Colors.Blue))},
+                            {CubeFace.U, new DiffuseMaterial(new SolidColorBrush(Colors.Yellow))},
+                        });
+
+                        group.Children.Add(c.group);
+                    }
+                }
+            }
+        }
+    }
+}
