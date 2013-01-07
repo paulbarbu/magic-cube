1e05d3005183ca98dac2236ee38efe63148e0e2f
Barbu Paul - Gheorghe <barbu.paul.gheorghe@gmail.com>
2013-01-07 23:35:35 +0200

    tweaked camera position and added the possibility to rotate the cube

--- magic-cube/MainWindow.xaml.cs
+++ magic-cube/MainWindow.xaml.cs
@@ -23,13 +23,68 @@ namespace magic_cube {
             InitializeComponent();
         }
 
-        private void Window_Loaded(object sender, RoutedEventArgs e) {
-            RubikCube c = new RubikCube(2, new Point3D(0, 0, 0), 3, 0.05);
+        Point start;
+        bool moveCamera = false;
+        Transform3DGroup rotations;
 
-            ModelVisual3D m;
-            m = new ModelVisual3D();
+        private void Window_Loaded(object sender, RoutedEventArgs e) {
+            int size = 3;
+            double edge_len = 1;
+            double space = 0.05;
+            double total_length = (edge_len + space) * size * 2;
+            double middle = ((edge_len + space) * size)/2;
+
+            RubikCube c = new RubikCube(size, new Point3D(-middle, -middle, -middle), edge_len, space);
+            
+            PerspectiveCamera camera = new PerspectiveCamera(
+                new Point3D(total_length, total_length, total_length),
+                new Vector3D(-total_length, -total_length, -total_length),
+                new Vector3D(0, 1, 0),
+                45
+            );
+
+            rotations = new Transform3DGroup();
+            c.group.Transform = rotations;
+
+            ModelVisual3D m = new ModelVisual3D();
             m.Content = c.group;
+
+            this.mainViewport.Camera = camera;
             this.mainViewport.Children.Add(m);
+        }
+        
+
+        private void Window_MouseRightButtonDown(object sender, MouseButtonEventArgs e) {
+            start = e.GetPosition(this);
+            moveCamera = true;
+            this.Cursor = Cursors.SizeAll;
+
+            Debug.Print("DOWN");
+        }
+
+        private void Window_MouseRightButtonUp(object sender, MouseButtonEventArgs e) {
+            moveCamera = false;
+            this.Cursor = Cursors.Arrow;
+            Debug.Print("UP");
+        }
+
+        private void Window_MouseMove(object sender, MouseEventArgs e) {
+            if (!moveCamera) {
+                return;
+            }
+
+            Point p = e.GetPosition(this);
+
+            double distX = p.X - start.X;
+            double distY = p.Y - start.Y;
+
+            start = p;
+
+            RotateTransform3D rotationX = new RotateTransform3D(new AxisAngleRotation3D(new Vector3D(1, 0, 0), distY), new Point3D(0, 0, 0));
+            RotateTransform3D rotationY = new RotateTransform3D(new AxisAngleRotation3D(new Vector3D(0, 1, 0), distX), new Point3D(0, 0, 0));
+
+            rotations.Children.Add(rotationX);
+            rotations.Children.Add(rotationY);
         }
     }
 }
