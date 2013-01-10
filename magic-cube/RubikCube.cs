2f4aff3b81584f4e491e434873116bfda94cb82e
Barbu Paul - Gheorghe <barbu.paul.gheorghe@gmail.com>
2013-01-10 07:46:11 +0200

    added the method getNextPossibleMoves which should translate the possible moves of a cube according to how the cube was modified

--- magic-cube/RubikCube.cs
+++ magic-cube/RubikCube.cs
@@ -103,7 +103,7 @@ namespace magic_cube {
         public void rotate(KeyValuePair<Move, RotationDirection> move, CubeFace f) {
             HashSet<Move> possibleMoves = new HashSet<Move>();
             Vector3D axis = new Vector3D();
-
+            
             foreach(Cube c in this.Children){
                 possibleMoves = c.possibleMoves;
                 possibleMoves.Remove((Move)f);
@@ -149,10 +149,90 @@ namespace magic_cube {
                             break;
                     }
 
-                    //c.rotations.Children.Add(new RotateTransform3D(new AxisAngleRotation3D(new Vector3D(0, 0, -1), 90 * Convert.ToInt32(move.Value)), new Point3D(0, 0, 0)));
                     c.rotations.Children.Add(new RotateTransform3D(new AxisAngleRotation3D(axis, 90 * Convert.ToInt32(move.Value)), new Point3D(0, 0, 0)));
+
+                    c.possibleMoves = getNextPossibleMoves(c.possibleMoves, move.Key, move.Value);
                 }
             }
+        }
+
+        //pass c.possibleMoves
+        private HashSet<Move> getNextPossibleMoves(HashSet<Move>moves, Move m, RotationDirection direction){
+            //TODO: S M E
+             Dictionary<Move, List<List<Move>>> substitutions = new Dictionary<Move, List<List<Move>>> {
+                {Move.F, new List<List<Move>>{
+                    new List<Move>{Move.U, Move.L, Move.U, Move.R},
+                    new List<Move>{Move.U, Move.R, Move.D, Move.R},
+                    new List<Move>{Move.D, Move.R, Move.D, Move.L},
+                    new List<Move>{Move.D, Move.L, Move.U, Move.L},
+                    new List<Move>{Move.U, Move.M, Move.E, Move.R},
+                    new List<Move>{Move.E, Move.R, Move.M, Move.D},
+                    new List<Move>{Move.M, Move.D, Move.L, Move.E},
+                    new List<Move>{Move.L, Move.E, Move.U, Move.M},
+                }},
+                {Move.U, new List<List<Move>>{
+                    new List<Move>{Move.B, Move.L, Move.B, Move.R},
+                    new List<Move>{Move.B, Move.R, Move.F, Move.R},
+                    new List<Move>{Move.F, Move.R, Move.F, Move.L},
+                    new List<Move>{Move.F, Move.L, Move.B, Move.L},
+                    new List<Move>{Move.B, Move.M, Move.S, Move.R},
+                    new List<Move>{Move.S, Move.R, Move.M, Move.F},
+                    new List<Move>{Move.M, Move.F, Move.L, Move.S},
+                    new List<Move>{Move.L, Move.S, Move.B, Move.M},
+                }},
+                {Move.L, new List<List<Move>>{
+                    new List<Move>{Move.B, Move.U, Move.F, Move.U},
+                    new List<Move>{Move.F, Move.U, Move.F, Move.D},
+                    new List<Move>{Move.F, Move.D, Move.B, Move.D},
+                    new List<Move>{Move.B, Move.D, Move.B, Move.U},
+                    new List<Move>{Move.S, Move.U, Move.E, Move.F},
+                    new List<Move>{Move.E, Move.F, Move.D, Move.S},
+                    new List<Move>{Move.D, Move.S, Move.B, Move.E},
+                    new List<Move>{Move.B, Move.E, Move.S, Move.U},
+                }},
+            };
+
+            List<List<Move>> l = new List<List<Move>>(substitutions[Move.F]);
+            foreach (List<Move> item in l)
+	        {
+		        item.Reverse();
+	        }
+            substitutions.Add(Move.B, l);
+
+            l = new List<List<Move>>(substitutions[Move.U]);
+            foreach (List<Move> item in l) {
+                item.Reverse();
+            }
+            substitutions.Add(Move.D, l);
+
+            l = new List<List<Move>>(substitutions[Move.L]);
+            foreach (List<Move> item in l) {
+                item.Reverse();
+            }
+            substitutions.Add(Move.R, l);
+
+            foreach (var s in substitutions[m]) {
+                if (direction == RotationDirection.ClockWise) {
+                    if (moves.Contains(s[0]) && moves.Contains(s[1])) {
+                        moves.Remove(s[0]);
+                        moves.Add(s[2]);
+
+                        moves.Remove(s[1]);
+                        moves.Add(s[3]);
+                    }
+                }
+                else {
+                    if (moves.Contains(s[2]) && moves.Contains(s[3])) {
+                        moves.Remove(s[2]);
+                        moves.Add(s[0]);
+
+                        moves.Remove(s[3]);
+                        moves.Add(s[1]);
+                    }
+                }
+            }
+
+            return moves;
         }
 
         private Dictionary<CubeFace, Material> setFaceColors(int x, int y, int z){
